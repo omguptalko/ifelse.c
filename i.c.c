e07ed1d6e5008a55a89f0b744aa5c6448aa7b54a
omguptalko <119674842+omguptalko@users.noreply.github.com>
2022-12-06 19:01:59 -0800

    Add files via upload

---
+++ i.c.c
@@ -0,0 +1,15 @@
+// program is else
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("enter the number:");
+scanf("%d", &a);
+if(a>10)
+{
+ printf("your number is greater than 10");
+}
+getch();
+}
